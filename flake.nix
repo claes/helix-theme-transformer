173522c1bdc84ef4d2622b5daa2eae6ada8f8929
claes <claes@holmerson.net>
2026-07-04 12:10:49 +0200

    Add flake

---
+++ flake.nix
@@ -0,0 +1,90 @@
+{
+  description = "Themeforge Helix theme semantic converter";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          common = {
+            pname = "themeforge";
+            version = "0.1.0";
+            src = builtins.path {
+              path = ./.;
+              name = "themeforge-source";
+            };
+            cargoHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+            nativeBuildInputs = [
+              pkgs.pkg-config
+            ];
+          };
+        in
+        {
+          default = pkgs.rustPlatform.buildRustPackage (
+            common
+            // {
+              doCheck = false;
+            }
+          );
+
+          tests = pkgs.rustPlatform.buildRustPackage (
+            common
+            // {
+              pname = "themeforge-tests";
+              doCheck = true;
+              cargoTestFlags = [
+                "--workspace"
+                "--all-targets"
+                "--all-features"
+              ];
+              installPhase = ''
+                runHook preInstall
+                mkdir -p "$out"
+                cp -R . "$out/checkout"
+                runHook postInstall
+              '';
+            }
+          );
+        }
+      );
+
+      checks = forAllSystems (system: {
+        default = self.packages.${system}.tests;
+      });
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.cargo
+              pkgs.clippy
+              pkgs.rust-analyzer
+              pkgs.rustc
+              pkgs.rustfmt
+            ];
+
+            RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+          };
+        }
+      );
+    };
+}
